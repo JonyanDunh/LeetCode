3ac13bcbd4a519c23423af6cd53dd795329189b0
JONYANDUNH <jonyandunh@outlook.com>
2023-03-06 10:37:24 +0800

    update

--- 5.longest-palindromic-substring.js
+++ 5.longest-palindromic-substring.js
@@ -10,7 +10,25 @@
  * @return {string}
  */
 var longestPalindrome = function(s) {
-    
+    var MaxString=""
+    var TmpString = s[0]
+    for (var index = 1; index <= s.length;) {
+        if (ReversedString.indexOf(TmpString) != -1) {
+            MaxString=((TmpString.length >= MaxString.length) ? TmpString : MaxString)
+            TmpString+=s[index]
+            index++
+        }
+        else
+        {
+            TmpString = TmpString.substring(1);
+        }
+        // console.log("The max string:",MaxString)
+    }
+    // MaxString=MaxString==""?TmpString:MaxString
+    return MaxString
 };
+function reverseArray(Array){
+    return Array.split('').reverse().join('');
+}
 // @lc code=end
 
